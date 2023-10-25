aaccfe7ee59fdd713ccf4ad8ca1fba85d1801eac
elPodemos <elias.poderr@gmail.com>
2023-10-25 14:53:33 +0200

    update

--- index.php
+++ index.php
@@ -88,143 +88,179 @@ head($page); ?>
     </section>
     <!-- End About Section -->
 
-    <section id="formule" class="section">
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <section id="doctors" class="doctors section">
+        <div class="container">
+
+            <div class="section-title">
+                <h2>IL NOUS ONT DÉJÀ REJOINS</h2>
+            </div>
+            <br>
+            <div class="row">
+
+                <div class="col-lg-6">
+                    <div class="member d-flex align-items-start">
+                        <div class="pic"><img src="images/testimonials-1.jpg" class="img-fluid" alt=""></div>
+                        <div class="member-info">
+                            <h4>Walter White</h4>
+                            <span>Chief Medical Officer</span>
+                            <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                            <div class="social">
+                                <a href=""><i class="ri-twitter-fill"></i></a>
+                                <a href=""><i class="ri-facebook-fill"></i></a>
+                                <a href=""><i class="ri-instagram-fill"></i></a>
+                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                <div class="col-lg-6 mt-4 mt-lg-0">
+                    <div class="member d-flex align-items-start">
+                        <div class="pic"><img src="images/testimonials-1.jpg" class="img-fluid" alt=""></div>
+                        <div class="member-info">
+                            <h4>Sarah Jhonson</h4>
+                            <span>Anesthesiologist</span>
+                            <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+                            <div class="social">
+                                <a href=""><i class="ri-twitter-fill"></i></a>
+                                <a href=""><i class="ri-facebook-fill"></i></a>
+                                <a href=""><i class="ri-instagram-fill"></i></a>
+                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 mt-4">
+                    <div class="member d-flex align-items-start">
+                        <div class="pic"><img src="images/testimonials-1.jpg" class="img-fluid" alt=""></div>
+                        <div class="member-info">
+                            <h4>William Anderson</h4>
+                            <span>Cardiology</span>
+                            <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
+                            <div class="social">
+                                <a href=""><i class="ri-twitter-fill"></i></a>
+                                <a href=""><i class="ri-facebook-fill"></i></a>
+                                <a href=""><i class="ri-instagram-fill"></i></a>
+                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 mt-4">
+                    <div class="member d-flex align-items-start">
+                        <div class="pic"><img src="images/testimonials-1.jpg" class="img-fluid" alt=""></div>
+                        <div class="member-info">
+                            <h4>Amanda Jepson</h4>
+                            <span>Neurosurgeon</span>
+                            <p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
+                            <div class="social">
+                                <a href=""><i class="ri-twitter-fill"></i></a>
+                                <a href=""><i class="ri-facebook-fill"></i></a>
+                                <a href=""><i class="ri-instagram-fill"></i></a>
+                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="card-deck">
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </section>
+
+    <section class="main section">
+        <div class="testimonials">
+            <input type="radio" name="testimonial" id="input-testimonial1" checked>
+            <input type="radio" name="testimonial" id="input-testimonial2">
+            <input type="radio" name="testimonial" id="input-testimonial3">
+            <input type="radio" name="testimonial" id="input-testimonial4">
+            <div class="testimonials-inner">
+                <div class="testimonial">
+                    <div class="testimonial-photo">
+                        <div class="photo-background"></div>
+                        <div class="photo-author"></div>
+                    </div>
+                    <div class="testimonial-text">
+                        <p>Wait a second... you're telling me this testimonials slider is powered solely by CSS? That's some next-level web wizardry! I'm sold! Give me that mind-blowing slider to my website!</p>
+                    </div>
+                    <div class="testimonial-author">Henry Schwengle</div>
+                </div>
+                <div class="testimonial">
+                    <div class="testimonial-photo">
+                        <div class="photo-background"></div>
+                        <div class="photo-author"></div>
+                    </div>
+                    <div class="testimonial-text">
+                        <p>The older I get, the more I understand why roosters just scream to start the day.</p>
+                    </div>
+                    <div class="testimonial-author">Mr. George Robert</div>
+                </div>
+                <div class="testimonial">
+                    <div class="testimonial-photo">
+                        <div class="photo-background"></div>
+                        <div class="photo-author"></div>
+                    </div>
+                    <div class="testimonial-text">
+                        <p>Two of my friends have never met each other. Before they spoke, I told both of them that the other is a bit deaf. They shouted at each other for a few minutes before they realized that I'm an asshole.</p>
+                    </div>
+                    <div class="testimonial-author">Doodle Wobblepants</div>
+                </div>
+                <div class="testimonial">
+                    <div class="testimonial-photo">
+                        <div class="photo-background"></div>
+                        <div class="photo-author"></div>
+                    </div>
+                    <div class="testimonial-text">
+                        <p>Turning on your lights and sirens after losing a drag race is just poor sportsmanship, man.</p>
+                    </div>
+                    <div class="testimonial-author">John "Vroom" Cena</div>
                 </div>
             </div>
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="testimonials-arrows">
+                <div class="arrow arrow-left">
+                    <label for="input-testimonial1"></label>
+                    <label for="input-testimonial2"></label>
+                    <label for="input-testimonial3"></label>
+                    <label for="input-testimonial4"></label>
+                    <span></span>
+                </div>
+                <div class="arrow arrow-right">
+                    <label for="input-testimonial1"></label>
+                    <label for="input-testimonial2"></label>
+                    <label for="input-testimonial3"></label>
+                    <label for="input-testimonial4"></label>
+                    <span></span>
                 </div>
             </div>
-        </div>
-
-
-        <div class="card-deck listFormule">
-            <div class="cardFormule">
-                <div class="contentFormule">
-                    <div class="backFormule">
-                        <div class="back-contentFormule">
-                            <i class="fa-solid fa-user fa-3xl" style="color: #ffffff;"></i>
-                            <strong>Abonnement</strong>
+            <div class="testimonials-bullets">
+                <label for="input-testimonial1">
+                    <div class="bullet">
+                        <div>
+                            <span></span>
                         </div>
                     </div>
-                    <div class="frontFormule">
-
-                        <div class="imgFormule">
-                            <div class="circleFormule">
-                            </div>
-                            <div class="circleFormule" id="rightFormule">
-                            </div>
-                            <div class="circleFormule" id="bottomFormule">
-                            </div>
-                        </div>
-
-                        <div class="front-contentFormule">
-                            <small class="badgeFormule">Pasta</small>
-                            <div class="descriptionFormule">
-                                <div class="titleFormule">
-                                    <p class="titleFormule">
-                                        <strong>Spaguetti Bolognese</strong>
-                                    </p>
-                                    <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
-                                        <g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997">
-                                            <g transform="scale(8,8)">
-                                                <path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                </div>
-                                <p class="card-footerFormule">
-                                    30 Mins &nbsp; | &nbsp; 1 Serving
-                                </p>
-                            </div>
+                </label>
+                <label for="input-testimonial2">
+                    <div class="bullet">
+                        <div>
+                            <span></span>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="cardFormule">
-                <div class="contentFormule">
-                    <div class="backFormule">
-                        <div class="back-contentFormule">
-                            <svg stroke="#ffffff" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" height="50px" width="50px" fill="#ffffff">
-
-                                <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-
-                                <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
-
-                                <g id="SVGRepo_iconCarrier">
-
-                                    <path d="M20.84375 0.03125C20.191406 0.0703125 19.652344 0.425781 19.21875 1.53125C18.988281 2.117188 18.5 3.558594 18.03125 4.9375C17.792969 5.636719 17.570313 6.273438 17.40625 6.75C17.390625 6.796875 17.414063 6.855469 17.40625 6.90625C17.398438 6.925781 17.351563 6.949219 17.34375 6.96875L17.25 7.25C18.566406 7.65625 19.539063 8.058594 19.625 8.09375C22.597656 9.21875 28.351563 11.847656 33.28125 16.78125C38.5 22 41.183594 28.265625 42.09375 30.71875C42.113281 30.761719 42.375 31.535156 42.75 32.84375C42.757813 32.839844 42.777344 32.847656 42.78125 32.84375C43.34375 32.664063 44.953125 32.09375 46.3125 31.625C47.109375 31.351563 47.808594 31.117188 48.15625 31C49.003906 30.714844 49.542969 30.292969 49.8125 29.6875C50.074219 29.109375 50.066406 28.429688 49.75 27.6875C49.605469 27.347656 49.441406 26.917969 49.25 26.4375C47.878906 23.007813 45.007813 15.882813 39.59375 10.46875C33.613281 4.484375 25.792969 1.210938 22.125 0.21875C21.648438 0.0898438 21.234375 0.0078125 20.84375 0.03125 Z M 16.46875 9.09375L0.0625 48.625C-0.09375 48.996094 -0.00390625 49.433594 0.28125 49.71875C0.472656 49.910156 0.738281 50 1 50C1.128906 50 1.253906 49.988281 1.375 49.9375L40.90625 33.59375C40.523438 32.242188 40.222656 31.449219 40.21875 31.4375C39.351563 29.089844 36.816406 23.128906 31.875 18.1875C27.035156 13.34375 21.167969 10.804688 18.875 9.9375C18.84375 9.925781 17.8125 9.5 16.46875 9.09375 Z M 17 16C19.761719 16 22 18.238281 22 21C22 23.761719 19.761719 26 17 26C15.140625 26 13.550781 24.972656 12.6875 23.46875L15.6875 16.1875C16.101563 16.074219 16.550781 16 17 16 Z M 31 22C32.65625 22 34 23.34375 34 25C34 25.917969 33.585938 26.730469 32.9375 27.28125L32.90625 27.28125C33.570313 27.996094 34 28.949219 34 30C34 32.210938 32.210938 34 30 34C27.789063 34 26 32.210938 26 30C26 28.359375 26.996094 26.960938 28.40625 26.34375L28.3125 26.3125C28.117188 25.917969 28 25.472656 28 25C28 23.34375 29.34375 22 31 22 Z M 21 32C23.210938 32 25 33.789063 25 36C25 36.855469 24.710938 37.660156 24.25 38.3125L20.3125 39.9375C18.429688 39.609375 17 37.976563 17 36C17 33.789063 18.789063 32 21 32 Z M 9 34C10.65625 34 12 35.34375 12 37C12 38.65625 10.65625 40 9 40C7.902344 40 6.960938 39.414063 6.4375 38.53125L8.25 34.09375C8.488281 34.03125 8.742188 34 9 34Z"></path>
-
-                                </g>
-
-                            </svg>
-                            <strong>Evenement</strong>
+                </label>
+                <label for="input-testimonial3">
+                    <div class="bullet">
+                        <div>
+                            <span></span>
                         </div>
                     </div>
-                    <div class="frontFormule">
-
-                        <div class="imgFormule">
-                            <div class="circleFormule">
-                            </div>
-                            <div class="circleFormule" id="rightFormule">
-                            </div>
-                            <div class="circleFormule" id="bottomFormule">
-                            </div>
-                        </div>
-
-                        <div class="front-contentFormule">
-                            <small class="badgeFormule">Pasta</small>
-                            <div class="descriptionFormule">
-                                <div class="titleFormule">
-                                    <p class="titleFormule">
-                                        <strong>Spaguetti Bolognese</strong>
-                                    </p>
-                                    <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
-                                        <g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997">
-                                            <g transform="scale(8,8)">
-                                                <path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                </div>
-                                <p class="card-footerFormule">
-                                    30 Mins &nbsp; | &nbsp; 1 Serving
-                                </p>
-                            </div>
+                </label>
+                <label for="input-testimonial4">
+                    <div class="bullet">
+                        <div>
+                            <span></span>
                         </div>
                     </div>
-                </div>
+                </label>
             </div>
         </div>
     </section>
--- js/script.js
+++ js/script.js
@@ -3,40 +3,41 @@
 /* Please ❤ this if you like it! */
 
 
-(function($) { "use strict";
+(function ($) {
+	"use strict";
 
-	$(function() {
+	$(function () {
 		var header = $(".start-style");
-		$(window).scroll(function() {    
+		$(window).scroll(function () {
 			var scroll = $(window).scrollTop();
-		
+
 			if (scroll >= 10) {
 				header.removeClass('start-style').addClass("scroll-on");
 			} else {
 				header.removeClass("scroll-on").addClass('start-style');
 			}
 		});
-	});		
-		
+	});
+
 	//Animation
-	
-	$(document).ready(function() {
+
+	$(document).ready(function () {
 		$('body.hero-anime').removeClass('hero-anime');
 	});
 
 	//Menu On Hover
-		
-	$('body').on('mouseenter mouseleave','.nav-item',function(e){
-			if ($(window).width() > 750) {
-				var _d=$(e.target).closest('.nav-item');_d.addClass('show');
-				setTimeout(function(){
-				_d[_d.is(':hover')?'addClass':'removeClass']('show');
-				},1);
-			}
-	});	
-	
+
+	$('body').on('mouseenter mouseleave', '.nav-item', function (e) {
+		if ($(window).width() > 750) {
+			var _d = $(e.target).closest('.nav-item'); _d.addClass('show');
+			setTimeout(function () {
+				_d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+			}, 1);
+		}
+	});
+
 	//Switch light/dark
-	
+
 	$("#switch").on('click', function () {
 		if ($("body").hasClass("dark")) {
 			$("body").removeClass("dark");
@@ -46,6 +47,7 @@
 			$("body").addClass("dark");
 			$("#switch").addClass("switched");
 		}
-	});  
-	
-  })(jQuery); 
+	});
+
+})(jQuery);
+
